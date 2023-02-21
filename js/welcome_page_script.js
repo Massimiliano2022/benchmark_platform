50163d2820f9dc56bd88ccd3e24196a92f9c75db
Massimiliano2022 <120369719+Massimiliano2022@users.noreply.github.com>
2023-02-21 14:38:43 +0100

    aggiunto messaggio errore in welcome page

--- js/welcome_page_script.js
+++ js/welcome_page_script.js
@@ -1,7 +1,12 @@
 function checkPromise() {
-    
+
     let promise = document.getElementById('promise');
+    let divForm = document.getElementById('input');
+    let paragrafoErrore = document.createElement('p');
     if(promise.checked){
         window.location.href = 'test.html';
+    }else{
+        paragrafoErrore.textContent = 'Devi necessariamente accettare le condizioni per procedere.'
+        divForm.appendChild(paragrafoErrore);
     }
 }
